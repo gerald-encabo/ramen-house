994a78004baa95bfb543bc1f60f082374c06597c
Gerald Encabo <geraldencabo@gmail.com>
2024-08-29 23:57:52 -0400

    Update application with AWS services

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -3,18 +3,17 @@ import Title from "@/components/Title/Title";
 import { BannerSection, BannerWrapper } from "./Banner.styled";
 
 const Banner = () => {
-
   return (
     <BannerSection>
-        <BannerWrapper>
-          <>
-           <Title heading="Best Ramen in GTA" />
-          </>
-          <p>Find the nearest Ramen House to You!</p>
-          <Button button="Start your Order" />
-        </BannerWrapper>
+      <BannerWrapper>
+        <>
+          <Title heading="Best Ramen in GTA" />
+        </>
+        <p>Find the nearest Ramen House to You!</p>
+        <Button button="Start your Order" />
+      </BannerWrapper>
     </BannerSection>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,24 +1,21 @@
 import { MenuProps, MenuDataType } from "@/types/TypeLists";
-import { menuList } from '@/assets/data/MenuLists'
+import { menuList } from "@/assets/data/MenuLists";
 import MenuCard from "@/components/Menu/MenuCard";
 import Title from "@/components/Title/Title";
 import { MenuContainer } from "./Menu.styled";
 
-const Menu = ({id, heading}: MenuProps) => {
-
+const Menu = ({ id, heading }: MenuProps) => {
   return (
     <MenuContainer id={id}>
-        <Title heading={heading} />
-        <div>
-          {
-            menuList.map((lists: MenuDataType, key: number) => (
-                <div key={key}>
-                  <MenuCard data={lists} prodCategory={heading}/>
-                </div>
-            ))
-          }
-        </div>
+      <Title heading={heading} />
+      <div>
+        {menuList.map((lists: MenuDataType, key: number) => (
+          <div key={key}>
+            <MenuCard data={lists} prodCategory={heading} />
+          </div>
+        ))}
+      </div>
     </MenuContainer>
-  )
-}
-export default Menu
+  );
+};
+export default Menu;
